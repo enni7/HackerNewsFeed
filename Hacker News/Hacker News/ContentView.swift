998aae06fc9478ca2336a770dbde5d0381e646db
enni <annaizzo7@hotmail.it>
2022-08-01 19:24:26 +0200

    First commit

---
+++ Hacker News/Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Anna Izzo on 01/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hacker News/Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Anna Izzo on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
