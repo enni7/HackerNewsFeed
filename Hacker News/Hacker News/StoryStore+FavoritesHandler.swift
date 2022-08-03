b76e45baed7bd9e01dec0dba5d09e2f6e6004144
enni <annaizzo7@hotmail.it>
2022-08-03 17:39:12 +0200

    Story store for handling API call, decoding and favorites

---
+++ Hacker News/Hacker News/StoryStore+FavoritesHandler.swift
@@ -0,0 +1,61 @@
+//
+//  StoryStore+FavoritesHandler.swift
+//  Hacker News
+//
+//  Created by Anna Izzo on 03/08/22.
+//
+
+import Foundation
+
+extension StoryStore {
+    
+    var fileURL: URL {
+        let docDir = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        return docDir.appendingPathComponent("FavoritesStoriesList.json")
+    }
+
+    func loadFavorites() throws {
+        let data = try Data(contentsOf: self.fileURL)
+        let decodedList = try JSONDecoder().decode(Array<Int>.self, from: data)
+        self.favoritesStoriesList = decodedList
+    }
+    
+    func saveFavorites() throws {
+        let encodedFavorites = try JSONEncoder().encode(self.favoritesStoriesList)
+        try encodedFavorites.write(to: self.fileURL, options: [.atomic])
+    }
+    
+    func save() {
+        do {
+            try saveFavorites()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func decodeSavedFavorites() async {
+        self.favoritesStories = []
+        for storyID in favoritesStoriesList {
+            await decodeAndAddItem(itemID: storyID, storyListType: .favorites)
+        }
+    }
+    
+    func addOrRemoveFavorite(item: Story){
+        if itemIsFavorite(item: item) {
+            self.favoritesStories = self.favoritesStories.filter({$0.id != item.id})
+            self.favoritesStoriesList = self.favoritesStoriesList.filter({$0 != item.id})
+        } else {
+            self.favoritesStories.append(item)
+            self.favoritesStoriesList.append(item.id)
+        }
+        save()
+    }
+    
+    func itemIsFavorite(item: Story) -> Bool {
+        if self.favoritesStoriesList.contains(item.id) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
---
+++ Hacker News/Hacker News/StoryStore.swift
@@ -0,0 +1,130 @@
+//
+//  StoryStore.swift
+//  Hacker News
+//
+//  Created by Anna Izzo on 03/08/22.
+//
+
+import Foundation
+
+public enum StoryListType: String, CaseIterable {
+    case newest = "News"
+    case top = "Top"
+    case bestStories = "Bests"
+    case favorites = "Favorites"
+    
+    var stringForURL : String? {
+        switch self {
+        case .newest:
+            return "newstories"
+        case .top:
+            return "topstories"
+        case .bestStories:
+            return "beststories"
+        default:
+            return nil
+        }
+    }
+}
+
+class StoryStore: ObservableObject {
+    
+    static let shared: StoryStore = StoryStore()
+    
+    
+    var storiesList = [Int]()
+    @Published var stories = [Story]()
+    
+    var favoritesStoriesList = [Int]()
+    @Published var favoritesStories = [Story]()
+        
+    var hasFinishedDecoding = false
+    var shouldStopDecoding = false
+    var isRefreshing = false {
+        didSet {
+            if !hasFinishedDecoding{
+                shouldStopDecoding = true
+            }
+        }
+    }
+    
+    private init() {
+
+    }
+            
+    func loadStoriesIDList(listType: StoryListType) async {
+
+        self.storiesList.removeAll()
+        
+        guard let stringTypeForUrl = listType.stringForURL,
+              let url = URL(string: "https://hacker-news.firebaseio.com/v0/\(stringTypeForUrl).json") else {
+            print("Invalid list url.")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            let list = try JSONDecoder().decode([Int].self, from: data)
+            
+            DispatchQueue.main.async {
+                
+                //--- Loads the first 100 items (IDs) of the list
+                self.storiesList.append(contentsOf: list.prefix(100))
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func decodeStoriesArray(for listType: StoryListType) async {
+        hasFinishedDecoding = false
+        self.stories.removeAll()
+        
+        for storyID in self.storiesList {
+            if shouldStopDecoding {
+                break
+            }
+            
+            await decodeAndAddItem(itemID: storyID, storyListType: listType)
+        }
+        
+        hasFinishedDecoding = true
+        shouldStopDecoding = false
+    }
+    
+    func decodeAndAddItem(itemID: Int, storyListType: StoryListType) async {
+        //--- URL of a single item
+        guard let url = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(itemID).json") else {
+            print("Invalid story json url.")
+            return
+        }
+
+        let urlRequest = URLRequest(url: url)
+        
+        //--- Decode
+        do {
+            let (data, _) = try await URLSession.shared.data(for: urlRequest)
+            let story : Story = try JSONDecoder().decode(Story.self, from: data)
+            
+            //--- Add the item to relative stories array
+            DispatchQueue.main.asyncAndWait(execute: DispatchWorkItem(block: {
+                
+                if storyListType == .favorites {
+                    self.favoritesStories.append(story)
+                } else {
+                    self.stories.append(story)
+                }
+            }))
+        } catch {
+            print("Error in decoding an item. \(error.localizedDescription)")
+        }
+    }
+    
+    func loadAndDecodeStoriesList(storyListType: StoryListType) {
+        Task {
+            await self.loadStoriesIDList(listType: storyListType)
+            await self.decodeStoriesArray(for: storyListType)
+        }
+    }
+}
