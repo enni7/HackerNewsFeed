6cc096f5e14f066bef39c328fc448406b8928c74
enni <annaizzo7@hotmail.it>
2022-08-03 17:13:53 +0200

    webView

---
+++ Hacker News/Hacker News/WebView.swift
@@ -0,0 +1,38 @@
+//
+//  WebView.swift
+//  Hacker News
+//
+//  Created by Anna Izzo on 01/08/22.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String
+    
+    var url : URL? {
+        return URL(string: urlString)
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = url {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+    
+    typealias UIViewType = WKWebView
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView(urlString: "https://thedissenter.org/jury-guilty-cia-programmer-schulte-vault-7-wikileaks/")
+    }
+}
