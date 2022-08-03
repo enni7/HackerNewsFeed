9f447c22e15fcf95e88230c539e84806efd5e1fd
enni <annaizzo7@hotmail.it>
2022-08-03 17:39:47 +0200

    content view and stories list view

--- Hacker News/Hacker News/ContentView.swift
+++ Hacker News/Hacker News/ContentView.swift
@@ -6,16 +6,79 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+
+    @StateObject var storyStore = StoryStore.shared
+    @State var storyListType: StoryListType = .newest
+    
+    @State var togglePullToRefresh = false
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack(spacing: 0){
+                
+                Text("Hacker News")
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .foregroundColor(.accentColor)
+                    .padding(.bottom)
+                
+                Picker(selection: $storyListType) {
+                    ForEach(StoryListType.allCases, id: \.self) { listType in
+                        Text(listType.rawValue)
+                    }
+                } label: {
+                    EmptyView()
+                }
+                .pickerStyle(.segmented)
+                
+                StoriesListView(storyListType: $storyListType)
+            }
+            .navigationBarHidden(true)
+        }
+        .onAppear{
+            loadStories()
+            loadFavorites()
+        }
+        .onChange(of: storyListType) { listType in
+            refreshStories(listType: listType)
+        }
+    }
+    
+    func refreshStories(listType: StoryListType){
+        if listType != .favorites {
+            storyStore.isRefreshing.toggle()
+            loadStories()
+        } else {
+            Task{
+                await storyStore.decodeSavedFavorites()
+            }
+        }
+    }
+    
+    func loadStories(){
+        Task{
+            await storyStore.loadStoriesIDList(listType: storyListType)
+            await storyStore.decodeStoriesArray(for: storyListType)
+        }
+    }
+    
+    func loadFavorites(){
+        do {
+            try storyStore.loadFavorites()
+        } catch {
+            print(error.localizedDescription)
+        }
+        Task{
+            await storyStore.decodeSavedFavorites()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().preferredColorScheme(.light)
     }
 }
---
+++ Hacker News/Hacker News/StoriesListView.swift
@@ -0,0 +1,55 @@
+//
+//  StoriesListView.swift
+//  Hacker News
+//
+//  Created by Anna Izzo on 03/08/22.
+//
+
+import SwiftUI
+
+struct StoriesListView: View {
+    @StateObject var storyStore = StoryStore.shared
+    @Binding var storyListType: StoryListType
+    
+    var storiesList: [Story] {
+        switch storyListType {
+        case .newest:
+            return storyStore.stories
+            
+        case .top:
+            return storyStore.stories
+            
+        case .bestStories:
+            return storyStore.stories
+            
+        case .favorites:
+            return storyStore.favoritesStories
+        }
+    }
+    
+    var body: some View {
+        List{
+            ForEach(storiesList){ story in
+                ItemView(item: story)
+                    .padding(.horizontal)
+                    .buttonStyle(.plain)
+            }
+        }
+        .listStyle(.plain)
+        .refreshable {
+            if storyListType == .favorites {
+                Task{ await storyStore.decodeSavedFavorites() }
+                
+            } else {
+                storyStore.isRefreshing.toggle()
+                storyStore.loadAndDecodeStoriesList(storyListType: storyListType)
+            }
+        }
+    }
+}
+
+struct StoriesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoriesListView(storyListType: .constant(.top))
+    }
+}
