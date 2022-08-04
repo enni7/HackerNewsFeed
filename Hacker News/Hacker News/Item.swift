0e9a63a3a32636f2ae1ed2d64d3821b61666d23f
enni <annaizzo7@hotmail.it>
2022-08-04 16:58:20 +0200

    item properties and func for comments

--- Hacker News/Hacker News/Item.swift
+++ Hacker News/Hacker News/Item.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Item : Codable, Identifiable, Equatable {
     let by : String?
@@ -14,15 +15,26 @@ struct Item : Codable, Identifiable, Equatable {
     let kids : [Int]?
     let score : Int?
     let time : Int?
+    var text : String?
     let title : String?
     let type : String?
     let url : String?
-    
+    var indentForComment: Int?
+        
     var formattedDate: String {
         if let time = time {
             let date = Date(timeIntervalSince1970: TimeInterval(time))
             let formattedDate = date.formatted(date: .abbreviated, time: .shortened)
             return formattedDate
+        } else {
+            return ""
+        }
+    }
+    
+    var timeAgoDate: String {
+        if let time = time {
+            let date = Date(timeIntervalSince1970: TimeInterval(time))
+            return date.timeAgo()
         } else {
             return ""
         }
@@ -38,7 +50,22 @@ struct Item : Codable, Identifiable, Equatable {
         }
     }
     
-    // An item example for previews
+    mutating func convertHTMLCommentText(){
+        if let text = self.text {
+            self.text = text.getHtmlFormattedString()
+        }
+    }
+
+    var cgFloatIndent : CGFloat {
+        guard let indentForComment = indentForComment else {return 0.0}
+        if indentForComment > 60 {
+            return CGFloat(60)
+        } else {
+            return CGFloat(indentForComment)
+        }
+    }
+
+    // Story example for previews
     static let storyExample = Item(
         by: "iamflimflam1",
         descendants: 73,
@@ -46,22 +73,36 @@ struct Item : Codable, Identifiable, Equatable {
         kids: [32130999, 32129798, 32130926, 32129758, 32131224, 32130112, 32130509, 32130506, 32130129, 32130935, 32130561, 32130437, 32130340, 32130131, 32130035, 32130785, 32129727],
         score: 122,
         time: 1658081948,
+        text: nil,
         title: "Picking up free lithium cells off the street and making them safe for use",
         type: "story",
         url: "https://www.atomic14.com/2022/07/16/free-lithium-cells.html")
+    
+    // Comment example for previews
+    static let commentExample = Item(
+        by: "norvig",
+        descendants: nil,
+        id: 2921983,
+        kids: [2922097, 2922429, 2924562, 2922709, 2922573, 2922140, 2922141],
+        score: 122,
+        time: 1658081948,
+        text: "Aw shucks, guys ... you make me blush with your compliments.<p>Tell you what, Ill make a deal: I'll keep writing if you keep reading. K?",
+        title: nil,
+        type: nil,
+        url: nil)
 }
 
 
 
-///* ------- STORY PROPERTIES LEGEND
-// id           The item's unique id.
-// type         The type of item. One of "job", "story" in our lists
-// by           The username of the item's author.
-// time         Creation date of the item, in Unix Time.
-// text         The comment, story or poll text. HTML.
-// kids         The ids of the item's comments, in ranked display order.
-// url          The URL of the story.
-// score        The story's score, or the votes for a pollopt.
-// title        The title of the story, poll or job. HTML.
-// descendants  In the case of stories or polls, the total comment count.
-// */
+/* ------- STORY PROPERTIES LEGEND
+ id           The item's unique id.
+ type         The type of item. One of "job", "story" in our lists
+ by           The username of the item's author.
+ time         Creation date of the item, in Unix Time.
+ text         The comment, story or poll text. HTML.
+ kids         The ids of the item's comments, in ranked display order.
+ url          The URL of the story.
+ score        The story's score, or the votes for a pollopt.
+ title        The title of the story, poll or job. HTML.
+ descendants  In the case of stories or polls, the total comment count.
+*/
